406cac1568eb2b8d829efbb8d91a25933c205d47
Depandre Singh <dipandersingh9632@gmail.com>
2023-06-04 07:05:14 +0530

    first commit

---
+++ src/main/java/com/cybersecure/tokenbasedapi/TokenBasedApiApplication.java
@@ -0,0 +1,13 @@
+package com.cybersecure.tokenbasedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenBasedApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenBasedApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cybersecure/tokenbasedapi/TokenBasedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersecure.tokenbasedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenBasedApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
