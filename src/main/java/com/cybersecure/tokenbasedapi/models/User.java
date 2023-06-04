0b14fb91853163999a9e3079ef39e645eb89a02a
Depandre Singh <dipandersingh9632@gmail.com>
2023-06-04 07:42:27 +0530

    Added Models

---
+++ src/main/java/com/cybersecure/tokenbasedapi/models/User.java
@@ -0,0 +1,29 @@
+package com.cybersecure.tokenbasedapi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    String name;
+    String username;
+    String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+                joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles;
+}
